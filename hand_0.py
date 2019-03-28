639be28e98d1ab0a06106aa2e78a7c9f86d9b9b8
Brian Beckman <bc.beckman@gmail.com>
2019-03-28 16:58:37 -0700

    cloned hands; some mysterious missing envs (e.g., adventure)

--- hand_0.py
+++ hand_0.py
@@ -1,14 +1,27 @@
 import gym
-env = gym.make("TwoHandsManipulateBlocks-v0")
+env = gym.make("TwoHandsManipulateBlocks-v0")  #
+
+# original was "HandManipulateBlock{}-v0, which is registered to
+# <-- gym.envs.robotics.HandBlockEnv in manipulate.py
+#
+# "TwoHandsManipulateBlocks-v0" started as a plumbed copy, registered to
+# <-- gym.envs.robotics:TwoHandsBlockEnvBBeckman in manipulate.py
+#
+# Search this repository for "bbeckman" to see my changes all over the place.
+
 # env = gym.make("HandManipulateBlock-v0")
 # env = gym.make("CartPole-v1")
 # env = gym.make("Zaxxon-v0")
 
 observation = env.reset()
-for _ in range(50):
-  env.render()
-  action = env.action_space.sample() # your agent here (this takes random actions)
-  observation, reward, done, info = env.step(action)
-  if done:
-    observation = env.reset()
+for _ in range(25):
+    # through core.py::Wrapper.render,
+    # hand_env.py::HandEnv.render
+    # robot_env.py::RobotEnv.render
+    env.render()
+    action = env.action_space.sample() # your agent here
+    # (this takes random actions)
+    observation, reward, done, info = env.step(action)
+    if done:
+        observation = env.reset()
 env.close()
