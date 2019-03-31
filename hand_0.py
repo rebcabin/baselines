73e398e0b8fd1ca030a4e5606acf097522260722
Brian Beckman <brianbeckman@pop-os.localdomain>
2019-03-31 10:15:00 -0700

    safety --- prep to change "observation_space" etc. in GoalEnv

--- hand_0.py
+++ hand_0.py
@@ -23,6 +23,7 @@ for _ in range(25):
     action = env.action_space.sample()  # your agent here
     # (this takes random actions)
     observation, reward, done, info = env.step(action)
+    _ = env.observation_space
     if done:
         observation = env.reset()
 env.close()
