ccb1e503b601e76780cc5e00c8b63e034d39eb8f
Brian Beckman <brianbeckman@pop-os.localdomain>
2019-04-03 07:36:52 -0700

    safety --- broken tests

--- test_hand_0.py
+++ test_hand_0.py
@@ -1,5 +1,8 @@
 import gym
 import numpy as np
+import scipy as sp
+import scipy.sparse as sparse
+
 env = gym.make("TwoHandsManipulateBlocks-v0")  # manipulate.py
 # env = gym.make("HandManipulateBlock-v0")
 
@@ -23,24 +26,37 @@ env = gym.make("TwoHandsManipulateBlocks-v0")  # manipulate.py
 
 _ = env.reset()
 
+
+# Abbreviations:
+# LRM :: Linear Regulator Matrix, #actions rows x #states columns
+# LRV :: Linear Regulator Vector,
+
+
 ONE_HAND_STATE_DIM = 48
 ONE_CUBE_STATE_DIM = 13
 ONE_SIDE_STATE_DIM = ONE_HAND_STATE_DIM + ONE_CUBE_STATE_DIM
 ONE_HAND_ACTION_DIM = 20
-ONE_LINEAR_REGULATOR_DIM = ONE_SIDE_STATE_DIM * ONE_HAND_ACTION_DIM
+LRV_DIM = ONE_SIDE_STATE_DIM * ONE_HAND_ACTION_DIM
 
 
-def lin_reg_matrix_from_lin_reg_vector(vec, new_shape):
-    pass
+LRM_SHAPE = (ONE_HAND_ACTION_DIM, ONE_SIDE_STATE_DIM)
+LRV_SHAPE = (LRV_DIM,)
 
 
-def lin_reg_vector_from_lin_reg_matrix(mat, new_shape):
-    pass
+def lrm_from_lrv(vec):
+    assert vec.shape == LRV_SHAPE
+    result = np.reshape(vec, LRM_SHAPE)
+    return result
+
+
+def lrv_from_lrm(mat):
+    assert mat.shape == LRM_SHAPE
+    result = np.reshape(mat, LRV_SHAPE)
+    return result
 
 
 def test_vec_from_mat():
-    mat = np.array([[1, 2, 3],
-                    [4, 5, 6]])
+    mat = np.array([[1, 2, 3], [4, 5, 6]])
     vec = np.reshape(mat, (6,))
     temp0 = vec == np.array([1, 2, 3, 4, 5, 6])
     result = np.all(temp0)
@@ -50,15 +66,35 @@ def test_vec_from_mat():
 def test_mat_from_vec():
     vec = np.array([1, 2, 3, 4, 5, 6])
     mat = np.reshape(vec, (2, 3))
-    temp0 = mat == np.array([[1, 2, 3],
-                             [4, 5, 6]])
+    temp0 = mat == np.array([[1, 2, 3], [4, 5, 6]])
     result = np.all(temp0)
     assert result
 
 
-def new_random_linear_regulator(sigma):
-    result = np.random
-    return result
+def new_spherical_LRV(mu, sigma):
+    assert mu.shape == LRV_SHAPE
+    # cov = sparse.identity(LRV_DIM, dtype=np.float32)
+    # cov = np.array(LRM_SHAPE, dtype=np.float32)
+    cov = np.array((42, 42), dtype=np.float32)
+    assert cov.shape == (42, 42)
+    assert LRM_SHAPE == (20, 61)
+    # git problem?
+    assert cov.shape == (20, 61)
+    # assert cov.shape == LRM_SHAPE
+    # for i in range(LRV_DIM):
+    #     cov.itemset((i, i), 1.0)
+    # assert cov.shape == LRV_DIM
+    return cov
+    # result = np.random.multivariate_normal(
+    #     mu, sigma * sparse.identity(LRV_DIM))
+    # return result
+
+
+def test_new_spherical_LRV():
+    temp0 = new_spherical_LRV(
+        np.zeros(LRV_SHAPE),
+        1.0
+    )
 
 
 def test_pytest_itself():
